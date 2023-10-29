5e4f8be314785087bb85e9eb16f3b62985aed3be
Banu Jafar <banujafarli9@gmail.com>
2023-10-29 18:20:01 +0400

    Connect to database with typeorm through Render provider

---
+++ server/src/entity/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, PrimaryGeneratedColumn, Entity, BaseEntity } from 'typeorm';
+
+@Entity()
+export class User  {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
