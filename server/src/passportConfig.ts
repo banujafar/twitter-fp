10f185914f7b2fe88eba1ae45a1203aae97b56bd
banujafar <69260401+banujafar@users.noreply.github.com>
2023-10-31 16:11:50 +0400

    Integrate Remember me functionality when logIn and create protected R… (#7)
    * Integrate Remember me functionality when logIn and create protected Route which can access only users which logged in
    
    * Change login form email field name and set token in cookie after registeration

--- server/src/passportConfig.ts
+++ server/src/passportConfig.ts
@@ -2,61 +2,70 @@ import { Strategy as LocalStrategy } from 'passport-local';
 import { User } from './entity/user.entity.ts';
 import bcrypt from 'bcrypt';
 import validator from 'validator';
+import { FindOptionsWhere } from 'typeorm';
+import { PassportStatic } from 'passport';
 
-const passportConfig = (passport) => {
+const passportConfig = (passport: PassportStatic) => {
   passport.use(
-    new LocalStrategy({ usernameField: 'email', passwordField: 'password' }, (email, password, done) => {
-      User.findOneBy({email})
-        .then((user) => {
-          if (!user) {
-            return done(null, false, { message: 'User not found' });
-          }
-
-          if (email && !validator.isEmail(email)) {
-            return done(null, false, { message: 'Incorrect email' });
-          }
-
-          bcrypt.compare(password, user.password, (err, result) => {
-            if (err) {
-              return done(err);
+    new LocalStrategy(
+      { usernameField: 'email', passwordField: 'password' },
+      (email: string, password: string, done) => {
+        User.findOneBy({ email })
+          .then((user) => {
+            if (!user) {
+              return done(null, false, { message: 'User not found' });
             }
-            if (result) {
-              return done(null, user);
-            } else {
-              return done(null, false, { message: 'Wrong password' });
+
+            if (email && !validator.isEmail(email)) {
+              return done(null, false, { message: 'Incorrect email' });
             }
-          });
-        })
-        .catch((err) => done(err));
-    }),
+
+            bcrypt.compare(password, user.password, (err, result) => {
+              if (err) {
+                return done(err);
+              }
+              if (result) {
+                return done(null, user);
+              } else {
+                return done(null, false, { message: 'Wrong password' });
+              }
+            });
+          })
+          .catch((err) => done(err));
+      },
+    ),
   );
-  passport.use('local-username',
-    new LocalStrategy({ usernameField: 'username', passwordField: 'password' }, (username, password, done) => {
-      User.findOneBy({username})
-        .then((user) => {
-          if (!user) {
-            return done(null, false, { message: 'User not found' });
-          }
-          bcrypt.compare(password, user.password, (err, result) => {
-            if (err) {
-              return done(err);
+  passport.use(
+    'local-username',
+    new LocalStrategy(
+      { usernameField: 'username', passwordField: 'password' },
+      (username: string, password: string, done) => {
+        User.findOneBy({ username })
+          .then((user) => {
+            if (!user) {
+              return done(null, false, { message: 'User not found' });
             }
-            if (result) {
-              return done(null, user);
-            } else {
-              return done(null, false, { message: 'Wrong password' });
-            }
-          });
-        })
-        .catch((err) => done(err));
-    }),
+            bcrypt.compare(password, user.password, (err, result) => {
+              if (err) {
+                return done(err);
+              }
+              if (result) {
+                return done(null, user);
+              } else {
+                return done(null, false, { message: 'Wrong password' });
+              }
+            });
+          })
+          .catch((err) => done(err));
+      },
+    ),
   );
 
-  passport.serializeUser((user, done) => {
-    done(null, user.id);
+  passport.serializeUser((user: User, done) => {
+    done(null, user);
   });
 
-  passport.deserializeUser((id, done) => {
+  passport.deserializeUser((id: FindOptionsWhere<User>, done) => {
     const user = User.findOneBy(id);
     done(null, user);
   });
