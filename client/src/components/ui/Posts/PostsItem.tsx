59060365791737843c0f7d152dae72618f811568
Banu Jafar <banujafarli9@gmail.com>
2023-11-28 14:34:00 +0400

    Separate posts into individual divs and include retweeted posts in user profiles

--- client/src/components/ui/Posts/PostsItem.tsx
+++ client/src/components/ui/Posts/PostsItem.tsx
@@ -10,32 +10,32 @@ const PostsItem: React.FC<{ postData: IUserPost }> = ({ postData }) => {
   const isRetweet = posts.some((post) => post.retweets?.some((retweet: any) => retweet.post.id === postData.id));
 
   return (
-    !isRetweet && (
-      <div className="tweet-container bg-white  w-full px-4">
-        <>
+    <>
+      {!isRetweet && (
+        <div className="tweet-container bg-white  w-full px-4">
           <SinglePost postData={postData} size={44} />
           <TweetActions postData={postData} />
-        </>
-        {!!postData.retweets?.length && (
-          <>
-            {postData.retweets?.map((retweet: any) => {
-              const retweetedPost = posts.find((post) => post.id === retweet.post.id);
-              return (
-                retweetedPost && (
-                  <div key={retweetedPost?.id}>
-                    <SinglePost postData={retweetedPost} size={44} />
-                    <div className="px-8 border rounded-xl">
-                      <SinglePost postData={postData} size={30} />
-                    </div>
-                    <TweetActions postData={retweetedPost} />
+        </div>
+      )}
+      {!!postData.retweets?.length && (
+        <>
+          {postData.retweets?.map((retweet: any) => {
+            const retweetedPost = posts.find((post) => post.id === retweet.post.id);
+            return (
+              retweetedPost && (
+                <div className="tweet-container bg-white  w-full px-4" key={retweetedPost?.id}>
+                  <SinglePost postData={retweetedPost} size={44} />
+                  <div className="px-8 border rounded-xl">
+                    <SinglePost postData={postData} size={30} />
                   </div>
-                )
-              );
-            })}
-          </>
-        )}
-      </div>
-    )
+                  <TweetActions postData={retweetedPost} />
+                </div>
+              )
+            );
+          })}
+        </>
+      )}
+    </>
   );
 };
 
