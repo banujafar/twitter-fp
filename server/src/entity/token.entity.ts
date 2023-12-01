675429835b731bee1264cc44ae3f509c2ed7d607
Banu Jafar <banujafarli9@gmail.com>
2023-12-01 17:23:23 +0400

    Modify script file

--- server/src/entity/token.entity.ts
+++ server/src/entity/token.entity.ts
@@ -1,18 +1,18 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Token extends BaseEntity {
-    @PrimaryGeneratedColumn('uuid')
-    tokenId: string;
+  @PrimaryGeneratedColumn('uuid')
+  tokenId: string;
 
-    @Column()
-    userId: number;
+  @Column({ type: 'bigint', nullable: true })
+  userId: number;
 
-    @Column('text')
-    token: string;
+  @Column('text')
+  token: string;
 
-    @Column({
-        type: "timestamp",
-    })
-    createdAt: Date;
+  @Column({
+    type: 'timestamp',
+  })
+  createdAt: Date;
 }
