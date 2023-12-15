36b60a90a6297c0580efcb0abc40b8b43db0369c
banujafar <69260401+banujafar@users.noreply.github.com>
2023-12-15 21:28:27 +0400

    Change images directory (#134)

--- client/src/components/ui/Aside.tsx
+++ client/src/components/ui/Aside.tsx
@@ -2,7 +2,7 @@ const Aside = () => {
   return (
     <>
       <aside className="w-3/5 h-screen sticky left-0 top-0 lg:flex md:hidden sm:hidden xs:hidden flex">
-        <img className="h-full max-w-full w-full object-cover" src="src/assets/images/aside.png" alt="" />
+        <img className="h-full max-w-full w-full object-cover" src="/assets/images/aside.png" alt="" />
       </aside>
     </>
   );
