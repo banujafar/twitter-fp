c987dbe0f72c43be510c5a5e8eaccd3e815df164
Banu Jafar <banujafarli9@gmail.com>
2023-12-15 18:42:33 +0400

    Improve search bar in post detail

--- client/src/pages/Post.tsx
+++ client/src/pages/Post.tsx
@@ -1,7 +1,11 @@
 import SearchBar from '../components/ui/Timeline/SearchBar';
 import PostsDetail from '../components/ui/Posts/PostsDetail';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
 
 const Post = () => {
+  const posts = useSelector((state: RootState) => state.post.post);
+  const users = useSelector((state: RootState) => state.user.users);
   return (
     <>
       <div className="mx-2 sm:mx-0 xs:mx-0 border border-gray-200 w-full min-h-screen">
@@ -9,7 +13,7 @@ const Post = () => {
       </div>
       <div className="flex flex-col mx-4 sm:hidden xs:hidden xxs:hidden md:hidden lg:flex xl:flex">
         <div className="sm:hidden xs:hidden xxs:hidden md:hidden lg:flex xl:flex">
-          <SearchBar searchedList={[]} />
+          <SearchBar searchedList={posts} searchedUsers={users} />
         </div>
       </div>
     </>
